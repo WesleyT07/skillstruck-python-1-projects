c92566ce9f0e27ce7aa87c5ed53f057539f5ffb5
WesleyT07 <138717387+WesleyT07@users.noreply.github.com>
2024-08-30 12:33:55 -0700

    exported from Skill Struck

---
+++ MAd_libs.py
@@ -0,0 +1,6 @@
+noun1 = input("Choose a noun:")
+pnoun1 = input('Choose a plural noun:')
+noun2 = input("Choose a noun:")
+place = input("Choose a place:")
+adjective = input("Choose an adjective (Describing word):")
+print('Did you know I have a pet ' + noun1 + '?' + " He likes to run around and play with all of the " + pnoun1 + '.' + ' One morning, I woke up and he was wearing a ' + noun2 + ' for a hat! I especially like to take him to the ' + place + ' because he shows off his ' + adjective + ' side.')
